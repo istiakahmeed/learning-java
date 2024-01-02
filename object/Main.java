1a5df88f7648f9895fa9fd84ae1aa76fc8a3805b
MINAL-AHMED <mimalahmed6219@gmail.com>
2024-01-03 01:29:56 +0600

    constructor method used

--- object/Main.java
+++ object/Main.java
@@ -1,11 +1,18 @@
 public class Main {
 
   public static void main(String[] args) {
-    Car myCar = new Car();
-    System.out.println(myCar.make);
-    System.out.println(myCar.model);
-    System.out.println(myCar.year);
-    myCar.drive();
-    myCar.breaks();
+    // Car myCar = new Car();
+    // System.out.println(myCar.make);
+    // System.out.println(myCar.model);
+    // System.out.println(myCar.year);
+    // myCar.drive();
+    // myCar.breaks();
+
+    // constructor method used
+
+    Human human = new Human("Minal", 22, 70);
+    Human human2 = new Human("Mubin", 18, 80);
+    human.eat();
+    human2.drink();
   }
 }
