92ddff2c6994574a1e8999c7f529edfe87d7b3fb
MINAL-AHMED <mimalahmed6219@gmail.com>
2024-01-06 19:26:57 +0600

    i learn java in next month

--- object/Main.java
+++ object/Main.java
@@ -1,5 +1,24 @@
 public class Main {
 
+  /**
+   * InnerMain
+   */
+  public class Car {
+
+    String name;
+    String model;
+    double year;
+
+    Car(String name, String model) {
+      this.name = name;
+      this.model = model;
+    }
+
+    void running() {
+      System.out.println(this.name + " is running");
+    }
+  }
+
   public static void main(String[] args) {
     // Car myCar = new Car();
     // System.out.println(myCar.make);
@@ -10,9 +29,10 @@ public class Main {
 
     // constructor method used
 
-    Human human = new Human("Minal", 22, 70);
-    Human human2 = new Human("Mubin", 18, 80);
-    human.eat();
-    human2.drink();
+    // Human human = new Human("Minal", 22, 70);
+    // Human human2 = new Human("Mubin", 18, 80);
+    // human.eat();
+    // human2.drink();
+
   }
 }
